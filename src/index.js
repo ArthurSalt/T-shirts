1f52fde9385099fedcb491c26c46d6034cff8feb
ArthurSalt <arthursalt1990@gmail.com>
2023-09-14 07:54:42 +0300

    test

--- src/index.js
+++ src/index.js
@@ -19,3 +19,15 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+
+//Git
+
+// To push a local branch to github type 'git push REMOTE_URL LOCAL_BRACH_NAME'
+// git push https://github.com/ArthurSalt/T-shirts.git master
+
+// Use 'git remote add CUSTOM_NAME REMOTE_URL' 
+// aaaato give a shorter name for remote URL for faster access.
+// git remote add origin https://github.com/ArthurSalt/T-shirts.git
