b6b799ba4960a3d758db496bb0fe557ca884cc6d
ArthurSalt <arthursalt1990@gmail.com>
2023-09-17 08:34:47 +0300

    pagination

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,22 +1,33 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Category from '../Category/Category';
 import Sort from '../Sort/Sort';
 import ItemCard from '../ItemCard/ItemCard';
 
-const Home = ({search}) => {
-   const [items, setItems] = React.useState([]);
-   const [categoryType, setCategoryType] = React.useState(0);
-   const [sortType, setSortType] = React.useState('name');
+import Pagination from '../Pagination/Pagination';
 
-   const url = 'https://64efad78219b3e2873c4c415.mockapi.io/items?' + 
-   `${categoryType ? `category=${categoryType}` : ''}` + 
-   `&sortBy=${sortType}`;
+const Home = ({ search }) => {
+   const [items, setItems] = useState([]);
+   const [categoryType, setCategoryType] = useState(0);
+   const [sortType, setSortType] = useState('name');
+   const [currentPage, setCurrentPage] = useState(1);
+   const [itemsPerPage, setItemsPerPage] = useState(10);
+
+   const lastItem = itemsPerPage * currentPage;
+   const firstItem = lastItem - itemsPerPage;
+   const currentItems = items.slice(firstItem, lastItem);
+
+
+   const url = 'https://64efad78219b3e2873c4c415.mockapi.io/items?' +
+      `${categoryType ? `category=${categoryType}` : ''}` +
+      `&sortBy=${sortType}`;
 
    useEffect(() => {
       fetch(url)
          .then((res) => res.json())
          .then((data) => setItems(data));
+
+      setCurrentPage(1)
    }, [categoryType, sortType, search])
 
    return (
@@ -28,12 +39,17 @@ const Home = ({search}) => {
          <section className='items'>
             <h1 className='items_list_name'>All T-Shirts</h1>
             <div className="items_list">
-               {items
-               .filter(obj => obj.name.toLowerCase().includes(search.toLowerCase()))
-               .map(obj =>
-                  <ItemCard key={obj.id} {...obj} />)}
+               {currentItems
+                  .filter(obj => obj.name.toLowerCase().includes(search.toLowerCase()))
+                  .map(obj =>
+                     <ItemCard key={obj.id} {...obj} />)}
             </div>
          </section>
+         <Pagination
+            items={items}
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
+            itemsPerPage={itemsPerPage} />
       </>
    );
 }
---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './Pagination.scss'
+
+const Pagination = ({items, currentPage, setCurrentPage, itemsPerPage}) => {
+   const pages = [];
+
+   for(let i = 1; i <= Math.ceil(items.length / itemsPerPage); i++) {
+      pages.push(i);
+   }
+
+   return ( 
+      <div className='pagination'>
+         <ul className='pages_row'>
+            {pages.map((num, i) => (
+               <li onClick={() => setCurrentPage(num)} className={currentPage === i + 1 ? 'active page' : 'page'} key={num}>{num}</li>
+            ))}
+         </ul>
+      </div>
+    );
+}
+ 
+export default Pagination;
